859b8d83770994092ecbc2aca3ab600014e3c1ad
TaishiMiyabe <75838736+TaishiMiyabe@users.noreply.github.com>
2021-02-16 00:28:27 +0900

    落ちてくる船を作成

--- Assets/new_Script/ObsController.cs
+++ Assets/new_Script/ObsController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obs : MonoBehaviour
+public class ObsController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,12 @@ public class Obs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //方針
+        //左中央右のいずれかに１つの障害物が生成されるのが上限
+        //前から迫ってくるものと、上から降ってくるもの
+        //上から降ってくるものに関しては、プレイヤーの進行に合わせたところに落とすようにする
+        //画面から出たものはdestroy
+        //衝突した時にもdestroy
+        //衝突してdestroyする際には何かしらのエフェクトをつける
     }
 }
---
+++ Assets/new_Script/ThrowedShipController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowedShipController : MonoBehaviour
+{
+    //火と煙と爆発
+    GameObject fire;
+    GameObject smoke;
+    GameObject explosion;
+    ParticleSystem fireParticle;
+    ParticleSystem smokeParticle;
+    ParticleSystem explosionParticle;
+    private bool isExplosion = false;
+
+    private Rigidbody shipRigidbody;
+
+    //飛んできた船の初期速度と初期回転速度
+    private float shipVelocityX = 0;
+    private float shipVelocityY = -3f;
+    private float shipVelocityZ = -50f;
+    private float shipRotationX = 0.15f;
+
+    //船の透明度の制御のため。
+    MeshRenderer shipMR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.shipRigidbody = GetComponent<Rigidbody>();
+
+        //火と煙
+        fire = GameObject.Find("FlamesParticleEffect");
+        smoke = GameObject.Find("SmokeEffect");
+        fireParticle = fire.GetComponent<ParticleSystem>();
+        smokeParticle = smoke.GetComponent<ParticleSystem>();
+
+
+        //縦回転
+        this.transform.Rotate(Random.Range(270, 280), 0, 0);
+
+        fireParticle.Play();
+        smokeParticle.Play();
+
+        //爆発は、道と接触した時に起こす
+        explosion = GameObject.Find("BigExplosionEffect");
+        explosionParticle = explosion.GetComponent<ParticleSystem>();
+
+        //船のMeshRendererを取得しておく(後で透明にするために)
+        shipMR = this.GetComponent<MeshRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(shipRotationX, 0, 0);
+
+        this.shipRigidbody.velocity = new Vector3(shipVelocityX, shipVelocityY, shipVelocityZ);
+
+        
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Road")
+        {
+            shipVelocityZ = 0;
+            shipRotationX = 0;
+
+            //道に接触したら爆発。同時に船は壊れた想定のため、透明に。
+            explosionParticle.Play();
+            shipMR.material.color = new Color(0, 0, 0, 1.0f);
+
+        }
+
+    }
+}
