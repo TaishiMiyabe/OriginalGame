b449bbf7413611c067c7c29919f65c1331cde5b4
TaishiMiyabe <75838736+TaishiMiyabe@users.noreply.github.com>
2021-02-22 00:42:21 +0900

    ゲームオーバー時のCameraController
    プレイヤーが走り続けているので、修正の必要あり

--- Assets/new_Script/CameraController.cs
+++ Assets/new_Script/CameraController.cs
@@ -9,6 +9,9 @@ public class CameraController : MonoBehaviour
     //カメラとプレイヤーの距離
     private float distance;
 
+    //gameover時のカメラのx角度
+    private float cameraRotate = 90;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +25,21 @@ public class CameraController : MonoBehaviour
     {
         //プレイヤーの走りに合わせてカメラを移動
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.player.transform.position.z - this.distance);
+
+        if(player.transform.position.y <= -1)
+        {
+            var target = Quaternion.Euler(new Vector3(cameraRotate, 0, 0));
+            var nowRotation = this.transform.rotation;
+
+            if(Quaternion.Angle(nowRotation, target) <= 1)
+            {
+                this.transform.rotation = target;
+            }
+            else
+            {
+                this.transform.Rotate(1, 0, 0);
+                this.transform.position = new Vector3(this.player.transform.position.x, this.player.transform.position.y + 5, this.player.transform.position.z);
+            }
+        }
     }
 }
